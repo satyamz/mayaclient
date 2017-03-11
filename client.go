cac770bc607e023c989f0054ac2c2bbae0a7accc
Satyam Zode <satyamzode@gmail.com>
2017-03-11 09:13:52 +0530

    Format go files using gofmt

--- client.go
+++ client.go
@@ -1,25 +1,25 @@
 package mayaclient
 
 import (
-        "io/ioutil"
-        "net/http"
+	"io/ioutil"
+	"net/http"
 )
 
 //Client : struct for maya client
 type Client struct {
-        URL string
+	URL string
 }
 
 //MayaClient : Client to connect to mayaserver and return response
 func (c *Client) MayaClient() string {
-        resp, err := http.Get(c.URL)
-        if err != nil {
-                panic(err)
-        }
-        defer resp.Body.Close()
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-                panic(err)
-        }
-        return string(body[:])
+	resp, err := http.Get(c.URL)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(body[:])
 }
